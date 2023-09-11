f1bceacadf22b6f6de6570f4b762974da8bffd5f
S2kDev <79189126+S2kDev@users.noreply.github.com>
2023-09-12 00:13:27 +0300

    [TOP/TFR] Work with variables
    Lesson 30, doing tasks from JS info.
    
    Foundation(s) path

---
+++ TOP/Foundation/lesson_30/index.js
@@ -0,0 +1,16 @@
+// Работа с переменными
+let admin;
+let adminName = 'Джон';
+
+admin = adminName;
+
+alert(admin); // => Джон
+
+// Придумайте правильные имена
+let ourPlanetName = 'Земля';
+let currentUserName = 'Джон';
+
+// Какие буквы (заглавные или строчные) использовать для имён констант?
+const BIRTHDAY = '18.04.1982';
+
+const age = someCode(BIRTHDAY);
