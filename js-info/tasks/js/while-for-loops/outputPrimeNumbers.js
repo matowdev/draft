91a36238971d78a4d964a1e9e74646e5f38741c6
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-10-29 13:59:49 +0300

    [js-info/tasks] Lesson "while-for-loops", 7 of 7
    Organizing the search for a "prime" number.
    
    draft

---
+++ js-info/tasks/js/while-for-loops/outputPrimeNumbers.js
@@ -0,0 +1,35 @@
+// Вывести простые числа
+// Напишите код, который выводит все простые числа из интервала от 2 до n.
+
+let arrNumbers = [];
+let number = arrNumbers;
+
+function outputPrimeNumbers(n) {
+  for (let num = 0; num <= n; num++) {
+    if (num > 1) {
+      arrNumbers.push(num);
+    }
+  }
+  console.log(arrNumbers);
+  console.log(number);
+
+  for (let i = 0; i < arrNumbers.length; i++) {
+    if (number[0] / arrNumbers[i] == 1) {
+      console.log('O da!');
+    }
+    // console.log(number[0] / arrNumbers[i]);
+  }
+}
+
+outputPrimeNumbers(10);
+
+// решение JS Info
+let n = 10;
+
+nextPrime: for (let i = 2; i <= n; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue nextPrime; // не подходит, берём следующее
+  }
+
+  console.log(i); // "простые" числа: 2, 3, 5, 7
+}
