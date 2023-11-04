c05e22bb337943d6a74510f1198de7b7eb21ac10
s2kdev <79189126+s2kdev@users.noreply.github.com>
2023-11-04 14:08:28 +0300

    [RET_js-info/tasks] Less "array-methods", 10 of 13
    Adding a few solutions from JS Info. Probability of combinations.
    
    Worth noting:
      - value of the Fisher-Yates algorithm.
    
    draft

--- js-info/tasks/js/array-methods/arrayShuffling.js
+++ js-info/tasks/js/array-methods/arrayShuffling.js
@@ -16,3 +16,49 @@ function shuffle(arr) {
 shuffle(numArr);
 
 // решение JS Info
+// простое решение
+function shuffle(array) {
+  array.sort(() => Math.random() - 0.5);
+}
+
+let arr = [1, 2, 3];
+shuffle(arr);
+console.log(arr);
+
+// или
+// тасование Фишера — Йетса
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+// подсчёт вероятности для всех возможных вариантов
+let count = {
+  123: 0,
+  132: 0,
+  213: 0,
+  231: 0,
+  321: 0,
+  312: 0,
+};
+
+for (let i = 0; i < 1000000; i++) {
+  let array = [1, 2, 3];
+  shuffle(array);
+  count[array.join('')]++;
+}
+
+// показать количество всех возможных вариантов
+for (let key in count) {
+  alert(`${key}: ${count[key]}`);
+}
+
+// Пример вывода:
+// 123: 166693
+// 132: 166647
+// 213: 166628
+// 231: 167517
+// 312: 166199
+// 321: 166316
